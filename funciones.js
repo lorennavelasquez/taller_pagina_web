751d23c6cdebf8901a7bfb45b794d4f946f68412
Lorenna Velasquez <lorevc@1986@gmail.com>
2024-05-21 20:55:45 -0500

    pagina terminada

---
+++ funciones.js
@@ -0,0 +1,14 @@
+const formularioContacto = document.getElementById("formularioContacto");
+
+const nombre = document.getElementById("nombre");
+
+const  Apellidos = document.getElementById('Apellidos');
+const  correo = document.getElementById('correo');
+const telefono = document.getElementById('telefono');
+const msg = document.getElementById('msg');
+
+formularioContacto.addEventListener('submit', function(){
+    alert('Se ha envíado el formulario');
+});
+
+
